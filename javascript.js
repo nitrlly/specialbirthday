13bf13667d08a6570786a446d1357907cc999eec
nitrlly <98016411+nitrlly@users.noreply.github.com>
2022-06-10 02:43:31 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,34 @@
+const bday = new Date("Jun 12, 2022 0:00:00");
+
+function updateCoundown(){ 
+
+    let time = bday.getTime();
+    var now = new Date().getTime();
+
+    var days = document.getElementById("days");
+    var hours = document.getElementById("hours");
+    var minutes = document.getElementById("minutes");
+    var seconds = document.getElementById("seconds");
+
+    var countdown = time-now;
+
+    if(countdown <= 0){
+        days.innerHTML = '00';
+        hours.innerHTML = '00';
+        minutes.innerHTML = '00';
+        seconds.innerHTML = '00';
+        document.getElementById("body").innerHTML='<img id="salad2" src="Images/salad.jpg" alt="salad"></img>' + '<p class="text">HAPPY BIRTHDAY</p>';
+    }
+
+    const d = Math.floor(countdown/1000/60/60/24);
+    const h = Math.floor(countdown/1000/60/60) % 24;
+    const m = Math.floor(countdown/1000/60) % 60;
+    const s = Math.floor(countdown/1000) % 60;
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+setInterval(updateCoundown, 1000);
